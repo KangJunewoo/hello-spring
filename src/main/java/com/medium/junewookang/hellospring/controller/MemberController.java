48476ad0e8fa7bec19671b1f3364f699f2574c98
KangJunewoo <brianjune.dev@gmail.com>
2021-05-27 11:38:45 +0900

    feat: DI 방법 3가지 정리
    결국 가장 좋은 건 생성자 주입

--- src/main/java/com/medium/junewookang/hellospring/controller/MemberController.java
+++ src/main/java/com/medium/junewookang/hellospring/controller/MemberController.java
@@ -7,7 +7,16 @@ import org.springframework.stereotype.Controller;
 @Controller // 빈으로 등록되어 스프링이 관리함.
 public class MemberController {
     // private final MemberService memberService = new MemberService(); 이렇게 쓰는거보다는
-    private final MemberService memberService;
+//    @Autowired final MemberService memberService; -> 이러한 필드주입은 가능하긴 하지만 별로댜.
+
+//    private MemberService memberService; -> 요런 세터주입도 가능하긴 하다고 함.
+//    @Autowired
+//    public void setMemberService(MemberService memberService){
+//        this.memberService = memberService;
+//    }
+
+    // 가장 권장되는 스타일은 생성자 주입.
+    private MemberService memberService;
 
     @Autowired // 스프링 컨테이너에서 가져오는 Service를 넣어주겠다.
     public MemberController(MemberService memberService){
