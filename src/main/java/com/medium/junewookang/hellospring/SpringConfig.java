f9dd287d2787841174670556720c4d506d3223c0
KangJunewoo <brianjune.dev@gmail.com>
2021-06-08 19:18:59 +0900

    feat: spring data jpa 도입해서 코드 극도로 단순화
    엄청 간단한 만큼 잘 알고 쓰는게 중요할 것이다!

--- src/main/java/com/medium/junewookang/hellospring/SpringConfig.java
+++ src/main/java/com/medium/junewookang/hellospring/SpringConfig.java
@@ -11,27 +11,14 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    // 컨트롤러는.. @Controller 달아줘야 한다.
-    // 사실 서비스랑 리포지토리도 @Service, @Repository 달아주면 알아서 관리되긴 하지만
-    // 여기선 그냥 시험삼아 여기서 아싸리 생성해서 빈에 넣어주는걸 보고 넘어가자.
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource, EntityManager em){
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-//        return new JpaMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
