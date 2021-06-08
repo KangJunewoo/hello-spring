0da04e4443fc2e04e44b0324cb000db7a41dc1f6
Junewoo Kang <brianjune.dev@gmail.com>
2021-06-09 00:23:59 +0900

    feat: 전통적인 jdbc 이용해 DB접근 및 테스트 구현 (#1)

--- src/main/java/com/medium/junewookang/hellospring/SpringConfig.java
+++ src/main/java/com/medium/junewookang/hellospring/SpringConfig.java
@@ -1,16 +1,25 @@
 package com.medium.junewookang.hellospring;
 
+
+import com.medium.junewookang.hellospring.repository.JdbcMemberRepository;
 import com.medium.junewookang.hellospring.repository.MemberRepository;
-import com.medium.junewookang.hellospring.repository.MemoryMemberRepository;
 import com.medium.junewookang.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
     // 컨트롤러는.. @Controller 달아줘야 한다.
     // 사실 서비스랑 리포지토리도 @Service, @Repository 달아주면 알아서 관리되긴 하지만
     // 여기선 그냥 시험삼아 여기서 아싸리 생성해서 빈에 넣어주는걸 보고 넘어가자.
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -19,6 +28,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+
     }
 }
